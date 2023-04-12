1b2af5106a4de06d388f653e72320defbc97b262
Sungwoong Kang <k900327s@gmail.com>
2023-04-12 11:13:24 +0900

    기본  ListView 사용법

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,35 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:toonflix/models/webtoon.dart';
 import 'package:toonflix/services/api_service.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  List<Webtoon> webtoons = [];
-  bool isLoading = true;
-
-  void waitForWebtoons() async {
-    webtoons = await ApiService.getTodaysToons();
-    isLoading = false;
-    setState(() {});
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    waitForWebtoons();
-  }
+  final Future<List<Webtoon>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
-    print(
-      webtoons,
-    );
-    print(isLoading);
     return Scaffold(
       appBar: AppBar(
         elevation: 2,
@@ -40,6 +18,24 @@ class _HomeScreenState extends State<HomeScreen> {
           '어늘의 웹툰',
           style: TextStyle(fontSize: 24, fontWeight: FontWeight.w400),
         ),
+      ),
+      body: FutureBuilder(
+        future: webtoons,
+        builder: (context, future) {
+          if (future.hasData) {
+            return ListView(
+              children: [
+                for (var webtoon in future.data!)
+                  Text(
+                    webtoon.title,
+                  ),
+              ],
+            );
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
       ),
     );
   }
